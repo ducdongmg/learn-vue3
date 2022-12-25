ebc02418efb10464bfecaefd3e4c34155a5f0b51
DongNguyen <ducdong.mg@gmail.com>
2022-12-25 16:55:45 +0900

    vue3 lession 2

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 [resource](https://youtu.be/AjWBhjGhq-w)
+
+#### 1. introduce
+#### 2. basic
+ - dùng cdn
+ - cách tạo vue js app bằng createApp và mount
+ - dùng data và template 
+ - click event
+ - conditional rendering. 
+     chỉ có v-if làm nhiệm vụ này, còn v-show thì chỉ ẩn hoặc hiện chứ nó ko control chuyện render trên dom
+     
+
--- main.js
+++ main.js
@@ -2,8 +2,18 @@
     const app = Vue.createApp({
         data() {
             return {
-                message: 'this is data'
+                message: 'this is data',
+                firstName: 'Dong',
+                lastName: 'Nguyen',
+                isShowName: true,
+                isShowHide: false
             }
-        }
+        },
+        methods: {
+            onChangeName() {
+                this.isShowName = !this.isShowName;
+                this.isShowHide = !this.isShowHide;
+            }
+        },
     });
     app.mount("#app");
