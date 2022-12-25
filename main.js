4ac9e000ada86cc611e035c12e738c3b1aebc714
DongNguyen <ducdong.mg@gmail.com>
2022-12-25 17:46:26 +0900

    lession 3

--- main.js
+++ main.js
@@ -6,7 +6,14 @@
                 firstName: 'Dong',
                 lastName: 'Nguyen',
                 isShowName: true,
-                isShowHide: false
+                isShowHide: false,
+
+                products: [
+                    {name: 'san pham A', price: 123},
+                    {name: 'san pham B', price: 321},
+                    {name: 'san pham C', price: 456},
+                ],
+                className: 'product'
             }
         },
         methods: {
@@ -15,5 +22,11 @@
                 this.isShowHide = !this.isShowHide;
             }
         },
+        computed: {
+            productComputed() {
+                return this.products.filter(pro => pro.price > 200);
+            }
+        }
+
     });
     app.mount("#app");
