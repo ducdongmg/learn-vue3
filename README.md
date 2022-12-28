6677a1cb3e53ab51e2bc002571103a71bbe8ea75
DongNguyen <ducdong.mg@gmail.com>
2022-12-28 19:44:57 +0900

    .editorconfig, global style

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 [resource](https://youtu.be/AjWBhjGhq-w)
 
 #### 1. introduce
-#### 2 & 3. basic
+#### 2 & [3. basic](https://youtu.be/uvKss7OSsRA)
  - dùng cdn
  - cách tạo vue js app bằng createApp và mount
  - dùng data và template 
@@ -9,7 +9,6 @@
  - conditional rendering. 
      chỉ có v-if làm nhiệm vụ này, còn v-show thì chỉ ẩn hoặc hiện chứ nó ko control chuyện render trên dom
 
-    [lesson](https://youtu.be/uvKss7OSsRA)
  - mouse event
  - v-for
  - v-bind
@@ -21,3 +20,9 @@
  - vue refs (reference): giúp component cha truy cập vào data của component con
    Khai báo tại nơi call component con: `<HelloWorld ref="hello"`
    Dùng tại js code `this.$refs.hello.msg`
+
+#### 4 [cli](https://youtu.be/bBiSjvPG7kA)
+ - Dùng template từ trang [boostrap](https://getbootstrap.com/docs/5.0/components/modal/)
+ - Cho editor tự động format indent dựa vào file `.editorconfig` với config lấy từ trang https://editorconfig.org/
+ - style `scoped`
+ - reset css của page dựa vào code từ https://meyerweb.com/eric/tools/css/reset/
--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <header-component />
-  <HelloWorld ref="hello" msg="Welcome to Your Vue.js App" />
-  <button @click="showInfo">Click to show console log</button>
+  <BoostrapModal />
+  <!-- <HelloWorld ref="hello" msg="Welcome to Your Vue.js App" /> -->
+  <!-- <button @click="showInfo">Click to show console log</button> -->
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
 import HeaderPage from "./components/HeaderPage.vue";
+import BoostrapModal from "./components/BoostrapModal.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    // HelloWorld,
     HeaderComponent: HeaderPage,
+    BoostrapModal,
   },
   methods: {
     showInfo() {
@@ -23,13 +26,4 @@ export default {
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
